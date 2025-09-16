b0a29502b1821f9592d6e4687bf162ebae37da09
Manon <audibertmanon@hotmail.com>
2025-09-16 21:54:59 +0200

    Refactor App component layout and remove unused styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,84 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col h-screen">
+        <div className="flex justify-end m-4 gap-4 text-black">
+          <p>EN</p>
+          <p>FR</p>
+        </div>
+        <div
+          id="header-logo"
+          className="flex items-center h-32 justify-center text-center text-5xl text-[#7c52b7] font-bold bg-[#fffbdd]"
+        >
+          <p>Mini Shop</p>
+        </div>
+
+        <div className="flex justify-end m-4">
+          <div
+            id="search-bar"
+            className="flex border-2 border-[#7c52b7] rounded-md pl-2 items-center"
+          >
+            <input aria-label="search-bar" type="text" placeholder="search" />
+          </div>
+        </div>
+
+        <div
+          id="menu-content"
+          className="flex flex-col text-[#7c52b7] font-semibold w-1/6 h-full p-4 m-4 border-r-2 border-[#7c52b7] gap-4"
+        >
+          <p>Filter</p>
+          <div className="flex flex-col gap-2">
+            <p>Category</p>
+            <span className="flex flex-col">
+              <label>
+                <input type="radio" name="home" value="home" />
+                Home
+              </label>
+              <label>
+                <input
+                  aria-label="category"
+                  type="radio"
+                  name="Tech"
+                  value="Tech"
+                />
+                Tech
+              </label>
+              <label>
+                <input
+                  aria-label="category"
+                  type="radio"
+                  name="Stationery"
+                  value="Stationery"
+                />
+                Stationery
+              </label>
+            </span>
+            <p>Price</p>
+            <span className="flex flex-col">
+              <label>
+                <input
+                  aria-label="ascending"
+                  type="radio"
+                  name="ascending"
+                  value="ascending"
+                />
+                ascending
+              </label>
+              <label>
+                <input
+                  aria-label="descending"
+                  type="radio"
+                  name="descending"
+                  value="descending"
+                />
+                descending
+              </label>
+            </span>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
